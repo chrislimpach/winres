c1af9886183952186b7334cd9449d737ea01f509
Christian Limpach <Christian.Limpach@gmail.com>
2025-02-03 00:11:09 +1000

    honour TARGET_RC when building with toolkit msvc

--- lib.rs
+++ lib.rs
@@ -652,15 +652,23 @@ impl WindowsResource {
     }
 
     fn compile_with_toolkit_msvc<'a>(&self, input: &'a str, output_dir: &'a str) -> io::Result<()> {
-        let rc_exe = PathBuf::from(&self.toolkit_path).join("rc.exe");
-        let rc_exe = if !rc_exe.exists() {
-            if cfg!(target_arch = "x86_64") {
-                PathBuf::from(&self.toolkit_path).join(r"bin\x64\rc.exe")
-            } else {
-                PathBuf::from(&self.toolkit_path).join(r"bin\x86\rc.exe")
+        let rc_exe = match std::env::var("TARGET_RC") {
+            Ok(rc_exe) => {
+                println!("cargo:rerun-if-env-changed=TARGET_RC");
+                PathBuf::from(rc_exe)
             }
-        } else {
-            rc_exe
+            Err(_) => {
+                let rc_exe = PathBuf::from(&self.toolkit_path).join("rc.exe");
+                if !rc_exe.exists() {
+                    if cfg!(target_arch = "x86_64") {
+                        PathBuf::from(&self.toolkit_path).join(r"bin\x64\rc.exe")
+                    } else {
+                        PathBuf::from(&self.toolkit_path).join(r"bin\x86\rc.exe")
+                    }
+                } else {
+                    rc_exe
+                }
+            }
         };
         println!("Selected RC path: '{}'", rc_exe.display());
         let output = PathBuf::from(output_dir).join("resource.lib");
